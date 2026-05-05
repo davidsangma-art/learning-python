02867805c8238757fc43ca23abda8d6d3290012a
davidsangma-art <davidsangma960@gmail.com>
2026-05-05 20:05:01 +0530

    Create todo_list.py

---
+++ todo_list.py
@@ -0,0 +1,4 @@
+todo=['Get quarters.','Do laundary.','Take a walk.','Get a haircut.','Make some tea.','Complete Lists chapter','Watch my hero academia']
+print(todo[0])
+print(todo[1])
+print(todo[2:5])
