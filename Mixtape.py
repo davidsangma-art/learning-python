bc3c224a72a2d3b17cb20d02369b98cf2bb58d1d
davidsangma-art <davidsangma960@gmail.com>
2026-05-06 12:11:19 +0530

    Create Mixtape.py

---
+++ Mixtape.py
@@ -0,0 +1,11 @@
+playlist=[
+  'Porches - rangerover',
+  'Mount Eerie - You Swan, Go on',
+  'Carolyn Polachek - Look at Me Now',
+  'Pinegrove - Darkness',
+  'LVL UP - Spirit Was',
+  'Mistski - First Love / Late Spring'
+]
+
+for song in playlist:
+  print(song)
