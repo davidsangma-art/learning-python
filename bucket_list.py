cb90bca03a44163992ef51a64225e42d60a68787
davidsangma-art <davidsangma960@gmail.com>
2026-05-06 12:22:51 +0530

    Create bucket_list.py

---
+++ bucket_list.py
@@ -0,0 +1,11 @@
+things_to_do=[
+  'Write a book.',
+  'Participate in at least one hackathon.',
+  'Build a simple daily routine.',
+  'Learn a few new phrases in another language.',
+  'Get an internship.',
+  'Build something people actually use.',
+]
+
+for item in things_to_do:
+  print(item)
