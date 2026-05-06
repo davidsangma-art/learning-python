069e81abc9bbbbe853e35fe60ae9ea2719979196
davidsangma-art <davidsangma960@gmail.com>
2026-05-06 13:07:49 +0530

    Create DNA.py

---
+++ DNA.py
@@ -0,0 +1,13 @@
+dna_sequence = ['GCT', 'AGC', 'AGG', 'TAA', 'ACT', 'CAT', 'TAT', 'CCC', 'ACG', 'GAA', 'ACC', 'GGA']
+
+item_to_find = 'TGA'
+item_found=False
+
+for item in dna_sequence:
+  if item==item_to_find:
+    item_found=True
+
+if item_found:
+  print("Item Found!")
+else:
+  print("Item not found")
