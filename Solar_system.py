afa2f10105228375ba94f225c4ef71c784f5ede6
davidsangma-art <davidsangma960@gmail.com>
2026-05-08 08:10:48 +0530

    Create Solar_system.py

---
+++ Solar_system.py
@@ -0,0 +1,30 @@
+# Write code below 💖
+from math import pi
+from random import choice as ch
+
+planets=[
+  'Mercury',
+  'Venus',
+  'Earth',
+  'Mars',
+  'Saturn'
+]
+
+random_planet=ch(planets)
+
+if random_planet=='Mercury':
+  r=2440
+elif random_planet=='Venus':
+  r=6052
+elif random_planet=='Earth':
+  r=6371
+elif random_planet=='Mars':
+  r=3390
+elif random_planet=='Saturn':
+  r=58232
+else:
+  print('Oops! An error occcured.')
+
+area=4*pi*r**2
+
+print(f"The surface of {random_planet} is approximately {round(area):,} Square Kilometer")
